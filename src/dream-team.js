f13ba64727bc1836993669f40be852771d7727f1
viktor <vitek230290@gmail.com>
2020-02-17 19:08:11 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  const result = [];
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  members.forEach( item => {
+    if ( typeof item === 'string') {
+      result.push(item.trim()[0].toUpperCase());
+    }
+  })
+
+  return result.sort().join('');
 };
