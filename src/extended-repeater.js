293e38089cd1fe0f47bc0953f1dc46ab05c72a7c
viktor <vitek230290@gmail.com>
2020-02-17 22:07:39 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let resultStr = '';
+
+    if (options.repeatTimes === undefined) {
+        resultStr =  `${str}${options.addition}`;
+    } else if (options.additionSeparator) {
+        const numberToSlice = options.separator.length * -1;
+        const numberToSliceAddStr = options.additionSeparator.length * -1;
+        const addStr = `${options.addition}${options.additionSeparator}`.repeat(options.additionRepeatTimes).slice(0,numberToSliceAddStr);
+        resultStr = `${str}${addStr}${options.separator}`.repeat(options.repeatTimes).slice(0,numberToSlice);
+    }  else if (options.addition) {
+        const numberToSlice = options.separator.length * -1;
+        resultStr =  `${str}${options.addition}${options.separator}`.repeat(options.repeatTimes).slice(0,numberToSlice); 
+    } else if (options.separator === undefined) {
+        options.separator = '+';
+        const numberToSlice = options.separator.length * -1;
+        resultStr =  `${str}${options.separator}`.repeat(options.repeatTimes).slice(0,numberToSlice);
+    } else if (options.separator) {
+        const numberToSlice = options.separator.length * -1;
+        resultStr =  `${str}${options.separator}`.repeat(options.repeatTimes).slice(0,numberToSlice);
+    }
+    return  resultStr;
+    
 };
-  
+
+
