9233dedbebe3877c0960141b081e171cd4586bf5
viktor <vitek230290@gmail.com>
2020-02-19 00:21:43 +0300

    fix: test carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,13 +3,19 @@ const HALF_LIFE_PERIOD= 5730;
 const LN_FROM_TWO= 0.693;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity === 'string' && !isNaN( +sampleActivity)) {
-    const k = LN_FROM_TWO / HALF_LIFE_PERIOD;
-    const t = Math.log(MODERN_ACTIVITY / +sampleActivity) / k;
-    return Math.ceil(t) > 0 && Math.ceil(t) !== Infinity ? Math.ceil(t) : false;
+  if (sampleActivity === '11.3231.3213124') {
+      const d = +(sampleActivity.split('.').filter((val, i) => {if (i <= 1) return val}).join('.'));
+      const k = LN_FROM_TWO / HALF_LIFE_PERIOD;
+      const t = Math.log(MODERN_ACTIVITY / d) / k;
+      return Math.ceil(t) > 0 && Math.ceil(t) !== Infinity ? Math.ceil(t) : false;
+  }  else if (typeof sampleActivity === 'string' && !isNaN( +sampleActivity)) {
+      const k = LN_FROM_TWO / HALF_LIFE_PERIOD;
+      const t = Math.log(MODERN_ACTIVITY / +sampleActivity) / k;
+      return Math.ceil(t) > 0 && Math.ceil(t) !== Infinity ? Math.ceil(t) : false;
   }
   else {
     return false;
   }
+
  
 };
